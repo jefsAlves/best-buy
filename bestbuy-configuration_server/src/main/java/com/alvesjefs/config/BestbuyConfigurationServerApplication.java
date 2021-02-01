699bf2ac1e57798eecbc680222b88621adcfc177
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-02-01 09:48:25 -0300

    configuration server

---
+++ bestbuy-configuration_server/src/main/java/com/alvesjefs/config/BestbuyConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.alvesjefs.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestbuyConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestbuyConfigurationServerApplication.class, args);
+	}
+
+}
